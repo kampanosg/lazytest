b3b8395848ef6727f255069ea191e37cd484911b
George Kampanos <gkabanos@icloud.com>
2024-03-02 17:07:17 +0000

    added unit tests for search

--- internal/tui/utils/search_test.go
+++ internal/tui/utils/search_test.go
@@ -3,12 +3,40 @@ package utils
 import (
 	"testing"
 
+	"github.com/kampanosg/lazytest/pkg/models"
 	"github.com/rivo/tview"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSearch(t *testing.T) {
 	t.Parallel()
+
+	vaderTests := &models.LazyTestSuite {
+		Path: "Darth Vader",
+		Tests: []*models.LazyTest{
+			{
+				Name: "TestVader",
+			},
+		},
+	}
+
+	sidiusTests := &models.LazyTestSuite{
+		Path: "Darth Sidius",
+		Tests: []*models.LazyTest{
+			{
+				Name: "TestSidius",
+			},
+		},
+	}
+
+	maulTests := &models.LazyTestSuite{
+		Path: "Darth Maul",
+		Tests: []*models.LazyTest{
+			{
+				Name:"TestMaul",
+			},
+		},
+	}
 
 	internal := tview.NewTreeNode("internal")
 	apprentices := tview.NewTreeNode("apprentices")
@@ -18,6 +46,10 @@ func TestSearch(t *testing.T) {
 	darthVader := tview.NewTreeNode("Darth Vader")
 	darthSidious := tview.NewTreeNode("Darth Sidious")
 	darthMaul := tview.NewTreeNode("Darth Maul")
+
+	darthVader.SetReference(vaderTests)
+	darthSidious.SetReference(sidiusTests)
+	darthMaul.SetReference(maulTests)
 
 	testTree := tview.NewTreeNode(".").
 		AddChild(internal.
@@ -35,16 +67,11 @@ func TestSearch(t *testing.T) {
 		query string
 		want  []*tview.TreeNode
 	}{
-		// {
-		// 	name:  "empty query",
-		// 	query: "",
-		// 	want:  []*tview.TreeNode{},
-		// },
-		// {
-		// 	name:  "no results",
-		// 	query: "luke",
-		// 	want:  nil,
-		// },
+		{
+			name:  "no results",
+			query: "luke",
+			want:  nil,
+		},
 		{
 			name:  "match",
 			query: "Vader",
@@ -52,13 +79,25 @@ func TestSearch(t *testing.T) {
 				darthVader,
 			},
 		},
+		{
+			name: "match multiple",
+			query: "Darth",
+			want: []*tview.TreeNode{
+				darthVader,
+				darthSidious,
+				darthMaul,
+			},
+		},
+		{
+			name: "strict uppercase",
+			query: "darth",
+			want: nil,
+		},
 	}
 	for _, tt := range tests {
-		tc := tt
-
-		t.Run(tc.name, func(t *testing.T) {
-			res := Search(testTree, tc.query)
-			assert.ElementsMatch(t, tc.want, res.GetChildren())
+		t.Run(tt.name, func(t *testing.T) {
+			res := Search(testTree, tt.query)
+			assert.ElementsMatch(t, tt.want, res.GetChildren())
 		})
 	}
 }
