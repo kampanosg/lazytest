ed227d27e754b97cf10be32ab5fab0677a1bb0ce
George Kampanos <gkabanos@icloud.com>
2024-03-10 15:36:00 +0000

    node changed tests

--- internal/tui/handlers/node_changed.go
+++ internal/tui/handlers/node_changed.go
@@ -21,22 +21,25 @@ func HandleNodeChanged(e *elements.Elements, s *state.State) func(node *tview.Tr
 
 		switch ref.(type) {
 		case *models.LazyTestSuite:
-			borderColor := tcell.ColorWhite
 			outputs := ""
+			hasFailedTest := false
 			for _, child := range node.GetChildren() {
 				res, ok := s.TestOutput[child]
 				if ok {
-					borderColor = tcell.ColorGreen
 					if !res.IsSuccess {
-						borderColor = tcell.ColorOrangeRed
+						hasFailedTest = true
 					}
-
 					output := fmt.Sprintf("--- %s ---\n%s\n\n", child.GetText(), res.Output)
 					outputs = outputs + output
 				}
 			}
-			e.Output.SetBorderColor(borderColor)
+
 			e.Output.SetText(outputs)
+			if hasFailedTest {
+				e.Output.SetBorderColor(tcell.ColorOrangeRed)
+			} else {
+				e.Output.SetBorderColor(tcell.ColorGreen)
+			}
 		case *models.LazyTest:
 			res, ok := s.TestOutput[node]
 			if ok {
