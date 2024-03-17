850c98fb2f3bf27008622dfa295a4caf666b994b
George Kampanos <gkabanos@icloud.com>
2024-03-17 09:36:44 +0000

    update help with layout changes

--- internal/tui/elements/help.go
+++ internal/tui/elements/help.go
@@ -3,7 +3,7 @@ package elements
 import "github.com/gdamore/tcell/v2"
 
 const helpText = `
-	[darkturquoise]1 / 2: [white]Focus on the tree / output 
+	[darkturquoise]1 / 2: [white]Focus on the tree / output
 	[darkturquoise]r: [white]Run the selected test / test suite
 	[darkturquoise]a: [white]Run all tests
 	[darkturquoise]f: [white]Run all failed tests
@@ -12,6 +12,9 @@ const helpText = `
 	[darkturquoise]Enter: [white](in search mode) Go to the search results
 	[darkturquoise]<ESC>: [white]Exit search mode
 	[darkturquoise]C: [white](outside search mode) Clear search
+	[darkturquoise]+: [white]Increase test panel size
+	[darkturquoise]-: [white]Decrease test panel size
+	[darkturquoise]0: [white]Reset layout
 	[darkturquoise]q: [white]Quit
 	[darkturquoise]?: [white]Show this help message
 `
