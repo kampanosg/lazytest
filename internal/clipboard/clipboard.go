cfa66f76ccf1dfa80cd3315fd7b6a94a47a0f2e8
George Kampanos <george.kampanos@nandos.co.uk>
2024-04-11 17:08:26 +0100

    format clipboard

--- internal/clipboard/clipboard.go
+++ internal/clipboard/clipboard.go
@@ -3,7 +3,7 @@ package clipboard
 import "github.com/atotto/clipboard"
 
 // ClipboardManager is a wrappen of atotto/clipboard so it can easily be used as paramater and mocked
-type ClipboardManager struct {}
+type ClipboardManager struct{}
 
 func NewClipboardManager() *ClipboardManager {
 	return &ClipboardManager{}
