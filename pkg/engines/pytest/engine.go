3d693c7ed637d6abb05bb22a40bf69fbc46f450b
George Kampanos <george.kampanos@nandos.co.uk>
2024-04-17 21:46:52 +0100

    created basic structure

---
+++ pkg/engines/pytest/engine.go
@@ -0,0 +1,20 @@
+package pytest
+
+import "github.com/kampanosg/lazytest/pkg/models"
+
+const icon = ""
+
+type PytestEngine struct {
+}
+
+func (p *PytestEngine) NewPytestEngine() *PytestEngine {
+	return &PytestEngine{}
+}
+
+func (p *PytestEngine) Icon() string {
+	return icon
+}
+
+func (p *PytestEngine) Load(dir string) (*models.LazyTree, error) {
+	return nil, nil
+}
